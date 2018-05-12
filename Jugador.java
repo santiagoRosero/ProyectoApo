09efb8c007f70789b99dc47c77f7bd5b79478c90
santiagoRosero <39218814+santiagoRosero@users.noreply.github.com>
2018-05-12 10:15:47 -0500

    jugador.java

--- Jugador.java
+++ Jugador.java
@@ -1,5 +1,21 @@
 package modelo;
 
 public class Jugador {
-
+  public final static String ORO="oro";
+  public final static String PLATA="plata;
+    public final static String BRONCE="bronce";
+ private String nombre;
+ private int puntaje;
+ private String categoria;
+ private int edad;
+ 
+  public Jugador(String name, int points, String catagoria, int edad){
+    nombre=name;
+    puntaje=points;
+    categoria=categoria;
+    edad=edad;
 }
+  
+  
+  
+  
